e3b5b376ac9e393bd0aef8196973d0578fdf6a5e
knud21kea <grah0070@stud.kea.dk>
2022-03-22 00:54:42 +0100

    added unit test for getAddedEmails

--- src/test/java/com/example/freemoneynoscam/services/ValidateEmailServiceTest.java
+++ src/test/java/com/example/freemoneynoscam/services/ValidateEmailServiceTest.java
@@ -1,6 +1,9 @@
 package com.example.freemoneynoscam.services;
 
+import com.example.freemoneynoscam.models.Email;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,7 +13,6 @@ class ValidateEmailServiceTest {
 
     @Test
     void isEmailValid() {
-        //Arrange
         String em1 = "nifr@kea.dk";
         String em2 = "nifr@dk";
         String em3 = ".@";
@@ -35,4 +37,33 @@ class ValidateEmailServiceTest {
         assertFalse(result6);
     }
 
+    @Test
+    void getAddedEmails() {
+        //Arrange
+        String e0 = "gh@mail.co.uk";
+        String e1 = "a@b.c";
+        String e2 = "grah0070@stud.kea.dk";
+        String e3 = "xxx@z.com";
+        String e4 = "me@isp.dk";
+        String e5 = "gh@mail.dk";
+        String e6 = "bb@cc.dd";
+        String e7 = "xyx@mail.se";
+        String e8 = "zzz@z.net";
+        String e9 = "a@info.net";
+
+        //Act
+        ArrayList<Email> emails = service.getAddedEmails();
+
+        //Assert
+        assertEquals(e0,emails.get(0).getEmailAddress());
+        assertEquals(e1,emails.get(1).getEmailAddress());
+        assertEquals(e2,emails.get(2).getEmailAddress());
+        assertEquals(e3,emails.get(3).getEmailAddress());
+        assertEquals(e4,emails.get(4).getEmailAddress());
+        assertEquals(e5,emails.get(5).getEmailAddress());
+        assertEquals(e6,emails.get(6).getEmailAddress());
+        assertEquals(e7,emails.get(7).getEmailAddress());
+        assertEquals(e8,emails.get(8).getEmailAddress());
+        assertEquals(e9,emails.get(9).getEmailAddress());
+    }
 }
